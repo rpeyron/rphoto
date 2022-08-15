34c1b3cc0ffe521e5a937c145425c5d634074d3a
rpeyron <remi+github@via.ecp.fr>
2022-08-15 19:37:31 +0200

    converted to 7z

--- build/win32/make_zip.bat
+++ build/win32/make_zip.bat
@@ -1,11 +1,17 @@
 REM call upx_compress.bat
 cd ..\..\bin
 mkdir ..\distrib
-zip -9 -r -j rphoto.htb ..\doc\use
-zip -9 ..\distrib\rphoto_bin.zip RPhoto.exe jpegtran.exe jhead.exe wrjpgcom.exe rphoto.htb lang\fr\RPhoto.mo lang\cs\RPhoto.mo lang\ru\RPhoto.mo
-cd ..\lib
-zip -9 -r ..\distrib\wxrecttracker.zip wxRectTracker
-zip -9 -r ..\distrib\wxmisc.zip wxmisc
-zip -9 -r ..\distrib\wxjpegexifhanfler.zip wxJPEGExifHandler
+
+@REM zip -9 -r -j rphoto.htb ..\doc\use
+@REM zip -9 ..\distrib\rphoto_bin.zip RPhoto.exe jpegtran.exe jhead.exe wrjpgcom.exe rphoto.htb lang\fr\RPhoto.mo lang\cs\RPhoto.mo lang\ru\RPhoto.mo
+@REM cd ..\lib
+@REM zip -9 -r ..\distrib\wxrecttracker.zip wxRectTracker
+@REM zip -9 -r ..\distrib\wxmisc.zip wxmisc
+@REM zip -9 -r ..\distrib\wxjpegexifhanfler.zip wxJPEGExifHandler
 
 
+7z a -tzip -r rphoto.htb ..\doc\use\*.*
+7z a -tzip -r  ..\distrib\rphoto_bin.zip RPhoto.exe jpegtran.exe jhead.exe wrjpgcom.exe rphoto.htb lang\fr\RPhoto.mo lang\cs\RPhoto.mo lang\ru\RPhoto.mo
+7z a -tzip -r  ..\distrib\wxrecttracker.zip wxRectTracker
+7z a -tzip -r  ..\distrib\wxmisc.zip wxmisc
+7z a -tzip -r  ..\distrib\wxjpegexifhanfler.zip wxJPEGExifHandler
