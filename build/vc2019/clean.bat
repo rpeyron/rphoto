2eece29188b4a8e1834f392374b9eddd981da313
rpeyron <remi+github@via.ecp.fr>
2022-08-22 20:52:17 +0200

    Revert wrongly updated files

--- build/vc2019/clean.bat
+++ build/vc2019/clean.bat
@@ -1,8 +1,6 @@
 rmdir /s / q .vs
 for /d %%G in ("Debug*") do rd /s /q "%%~G"
 for /d %%G in ("Release*") do rd /s /q "%%~G"
-for /d %%G in ("x64*") do rd /s /q "%%~G"
-for /d %%G in (".vs") do rd /s /q "%%~G"
 del /q ..\..\bin\*.ilk
 del /q ..\..\bin\*.pdb
 del /q ..\..\bin\*.bsc
